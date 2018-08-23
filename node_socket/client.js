c41927728395da29f5be7ec5c04f8b2e0afc429b
LAPTOP-69GLBRMP\congmt <mactiencong@gmail.com>
2018-08-23 16:56:20 +0900

    update

--- node_socket/client.js
+++ node_socket/client.js
@@ -1,17 +1,37 @@
-var io = require('socket.io-client')
+var socketIOClient = require('socket.io-client')
 var fs = require('fs')
 var config = require('./config.js')()
 console.log(config)
-var socket = io.connect(config.bootstap_node, {reconnect: true})
+var bootstapNodeSocket = socketIOClient.connect(config.bootstap_node, {reconnect: true})
 var nodeAddresses = []
 
-socket.on('connect', function() {
+bootstapNodeSocket.on('connect', function() {
     console.log('connect')
-    socket.emit('NEW_NODE', {address: config.address, port: config.port})
-    socket.emit('GET_NODE_ADDRESS')
-    socket.on('NODE_ADDRESSES', (nodeAddresses)=>{
+    bootstapNodeSocket.emit('NEW_NODE', {address: config.address, port: config.port})
+    bootstapNodeSocket.emit('GET_NODE_ADDRESS')
+    bootstapNodeSocket.on('NODE_ADDRESSES', (nodeAddresses)=>{
         console.log(nodeAddresses)
         nodeAddresses = nodeAddresses
         fs.writeFile('nodes.json')
     })
 })
+
+var express = require('express')()
+var nodeServer = require('http').Server()
+var socketIO = require('socket.io')(nodeServer)
+
+express.post('/new_transaction', (transaction)=>{
+    console.log(transaction)
+    bootstapNodeSocket.emit('NEW_TRANSACTION', transaction)
+})
+
+express.post('/new_block', (block)=>{
+    console.log(block)
+    bootstapNodeSocket.emit('NEW_BLOCK', transaction)
+})
+
+
+nodeServer.listen(config.port, () => {
+    console.log("Listening on " + config.port)
+})
+nodeServer.post
